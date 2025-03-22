0abd6b2e64eadb1be3f4dfe54651b99a7f827429
ywyh <yassienwyh0@gmail.com>
2025-03-22 22:50:58 +0200

    fixing esling issues

--- src/app/info/[id]/page.tsx
+++ src/app/info/[id]/page.tsx
@@ -1,9 +1,6 @@
 import AnimeData from "@/app/info/[id]/_components/data";
 
-export default async function Info({ params }: { params: { id: string } }) {
-    const resolvedParams = await params;
-    const id = resolvedParams.id;
-
+export default async function Info({ params: { id } }: { params: { id: string } }) {
     return (
       <div>
         <AnimeData id={id} />
--- src/app/subs/[id]/[ep]/page.tsx
+++ src/app/subs/[id]/[ep]/page.tsx
@@ -1,10 +1,6 @@
 import Subs from "@/app/subs/[id]/[ep]/_components/subs";
 
-export default async function Watch({ params }: { params: { id: string, ep: string } }) {
-    const resolvedParams = await params;
-    const id = resolvedParams.id;
-    const ep = resolvedParams.ep;
-
+export default async function Watch({ params: { id, ep } }: { params: { id: string, ep: string } }) {
     return (
       <div className="container mx-auto px-4 py-6">
         <Subs id={id} ep={ep} />
